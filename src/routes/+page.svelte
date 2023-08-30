0df4cd93058ee495069d720fd576f96f45cbce5c
Sriraj Bura <sriraj.bura16@gmail.com>
2023-08-30 16:03:58 -0400

    implement world map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+
+	onMount(() => {
+		const svg = d3.select('svg');
+		const width = +svg.attr('width');
+		const height = +svg.attr('height');
+
+		const projection = d3.geoNaturalEarth1().fitSize([width, height], { type: 'Sphere' });
+
+		d3.json(
+			'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson'
+		).then((data) => {
+			// Draw the map
+			svg
+				.append('g')
+				.selectAll('path')
+				.data(data.features)
+				.enter()
+				.append('path')
+				.attr('fill', '#69b3a2')
+				.attr('d', d3.geoPath().projection(projection))
+				.style('stroke', '#fff')
+				.on('mouseover', function (event, d) {
+					d3.select(this).attr('fill', '#8EC5B6');
+				})
+				.on('mouseout', function (event, d) {
+					d3.select(this).attr('fill', '#69b3a2');
+				})
+				.on('click', (event, d) => {
+					console.log(d);
+					console.log(d.properties.name);
+				});
+		});
+	});
+</script>
+
+<svg width="800" height="800" />
