c11d954d2738f08d1081afcf8e06c3b22128a2d8
Sriraj Bura <sriraj.bura16@gmail.com>
2023-09-06 17:52:42 -0400

    deployment test 2

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,36 +1,36 @@
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
-const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY
-});
+// const openai = new OpenAI({
+// 	apiKey: process.env.OPENAI_API_KEY
+// });
 
-export async function load() {
-	const response = await openai.chat.completions.create({
-		model: 'gpt-3.5-turbo',
-		messages: [
-			{
-				role: 'system',
-				content: `Generate an intriguing question related to a country -- on various topics. The answer should always equal to the coutry. Structure the response in a json object, for example:
-                    {
-                    "question": "Which nation is renowned for its ancient civilization that built massive pyramids along the Nile River?",
-                    "country_name": "Egypt"
-                    }
-                    The question should reflect only one country and the country should not be revealed in the question.
-                    `
-			}
-		],
-		temperature: 1.5,
-		max_tokens: 200,
-		top_p: 1,
-		frequency_penalty: 0.5,
-		presence_penalty: 0.5
-	});
+// export async function load() {
+// 	const response = await openai.chat.completions.create({
+// 		model: 'gpt-3.5-turbo',
+// 		messages: [
+// 			{
+// 				role: 'system',
+// 				content: `Generate an intriguing question related to a country -- on various topics. The answer should always equal to the coutry. Structure the response in a json object, for example:
+//                     {
+//                     "question": "Which nation is renowned for its ancient civilization that built massive pyramids along the Nile River?",
+//                     "country_name": "Egypt"
+//                     }
+//                     The question should reflect only one country and the country should not be revealed in the question.
+//                     `
+// 			}
+// 		],
+// 		temperature: 1.5,
+// 		max_tokens: 200,
+// 		top_p: 1,
+// 		frequency_penalty: 0.5,
+// 		presence_penalty: 0.5
+// 	});
 
-	console.log(response.json());
+// 	console.log(response.json());
 
-	const content = response.choices[0].message.content;
+// 	const content = response.choices[0].message.content;
 
-	const data = JSON.parse(content);
+// 	const data = JSON.parse(content);
 
-	return data;
-}
+// 	return data;
+// }
